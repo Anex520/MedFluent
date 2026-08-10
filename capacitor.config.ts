b1261bb58f5e0ec6a3629f544ec99f97afd46acf
Anex Favour <anexiroma2010@gmail.com>
2026-08-10 22:08:25 +0000

    ci: add GitHub APK build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.andex.medfluent',
+  appName: 'MedFluent',
+  webDir: 'dist'
+};
+
+export default config;
